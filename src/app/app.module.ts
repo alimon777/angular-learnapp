5681c61d61f9b8a3bcb6569867992c51a71be3da
alimon777 <alimonna777@gmail.com>
2024-10-26 15:35:15 +0530

    added http components for crud operation on productapi mysql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DataComponent } from './data/data.component';
 import { BirdsComponent } from './birds/birds.component';
+import { HttpComponent } from './http/http.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataComponent,
-    BirdsComponent
+    BirdsComponent,
+    HttpComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,25 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Product } from './Product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  getProducts():Product[] {
+  private products: Product[] = [];
+  product:Product = new Product(0,'','',0,0);
+
+  constructor(private http: HttpClient) { 
+    this.products = [
+      new Product(1021, 'Tata', 'Dish TV', 10, 2500),
+      new Product(3023, 'Wipro', 'Blubs', 210, 300),
+      new Product(3421, 'TVS', 'Belts', 210, 5500),
+      new Product(21, 'Reliance', 'Dish TV', 50, 4500),
+      new Product(432, 'ActFiber', 'Router', 60, 1400),
+      new Product(909, 'Samsung', 'Smart TV', 50, 72500),
+      new Product(654, 'LG', 'Airconditioners', 20, 52500),
+      new Product(675, 'Crompton', 'Ceiling Fans', 25, 1500),
+      new Product(2901, 'Carrier', 'Airconditioners', 5, 50500),
+      new Product(721, 'Usha', 'Table fans', 40, 1200),
+      new Product(454, 'One+', 'Mobiles', 50, 82500)
+    ];
+  }
+
+  getProducts(): Product[] {
     return this.products;
   }
-  private products: Product[] = [];
-  constructor() { 
-    this.products[0] = new Product(1021, 'Tata', 'Dish TV', 10, 2500);
-    this.products[1] = new Product(3023, 'Wipro', 'Blubs', 210, 300);
-    this.products[2] = new Product(3421, 'TVS', 'Belts', 210, 5500);
-    this.products[3] = new Product(21, 'Reliance', 'Dish TV', 50, 4500);
-    this.products[4] = new Product(432, 'ActFiber', 'Router', 60, 1400);
-    this.products[5] = new Product(909, 'Samsung', 'Smart TV', 50, 72500);
-    this.products[6] = new Product(654, 'LG', 'Airconditioners', 20, 52500);
-    this.products[7] = new Product(675, 'Crompton', 'Ceiling Fans', 25, 1500);
-    this.products[8] = new Product(2901, 'Carrier', 'Airconditioners', 5, 50500);
-    this.products[9] = new Product(721, 'Usha', 'Table fans', 40, 1200);
-    this.products[10] = new Product(454, 'One+', 'Mobiles', 50, 82500);
+
+  fetchAllProducts(): Observable<Product[]> {
+    const url: string = 'http://localhost:8090/product/api1.0/all';
+    return this.http.get<Product[]>(url);
+  }
+
+  addProduct(product:Product):Observable<any>{
+    const url: string = 'http://localhost:8090/product/api1.0/add';
+    return this.http.post(url,product);
+  }
+
+  deleteProduct(id:number):Observable<any>{
+    const url: string = 'http://localhost:8090/product/api1.0/delete/'+id;
+    return this.http.delete(url);
+  }
+
+  updateProduct(product:Product):Observable<any>{
+    const url: string = 'http://localhost:8090/product/api1.0/add';
+    return this.http.put(url,product);
   }
 }
